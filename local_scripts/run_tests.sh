3a978075c97ac69b47a047dd8ba0c58069018b7d
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-01-03 02:14:50 +0100

    Fixed run_tests scripy and added basic test suite

--- local_scripts/run_tests.sh
+++ local_scripts/run_tests.sh
@@ -3,3 +3,6 @@
 set -e
 
 echo "Running tests."
+
+cd ../tests
+for f in *.py; do python "$f"; done
---
+++ tests/start_page_e2e.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class StartPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_should_login_to_page_with_admin_account(self):
+        driver = self.driver
+        driver.get("http://localhost:3000")
+        self.assertIn("LOT Airlines", driver.title)
+        elem = driver.find_element_by_id("login-input")
+        elem.send_keys("admin")
+        elem = driver.find_element_by_id("password-input")
+        elem.send_keys("admin")
+        elem.send_keys(Keys.RETURN)
+        driver.find_element_by_class_name("login-button").click()
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".toolbar-header")))
+
+if __name__ == "__main__":
+    unittest.main()
